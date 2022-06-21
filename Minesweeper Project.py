8403456204dc8e9f8180faf18150223a4b4e90ea
nagham-mekky <nagham.mekky@aucegypt.edu>
2022-06-21 10:11:02 +0200

    Created all frames, added settings and utilities files

--- Minesweeper Project.py
+++ Minesweeper Project.py
@@ -1,12 +1,12 @@
 from tkinter import *
 import settings
-
+import utils
 def rgb_hack(rgb):
     return "#%02x%02x%02x" % rgb 
 #Instantiating window instance\
 root = Tk() #this is a regular window
 #Override settings of window
-root.configure(bg=rgb_hack((230, 230, 250))) #Add choose colour background
+root.configure(bg=rgb_hack((0, 0, 0))) #Add choose colour background
 root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title("Minesweeper Project")
 root.resizable(False, False)#It is all resizable, so we prevent it from that
@@ -15,8 +15,8 @@ root.resizable(False, False)#It is all resizable, so we prevent it from that
 top_frame = Frame( #Has title
     root,
     bg=rgb_hack((192, 192, 242)), #Change later
-    width =1440, #same width of window
-    height = 90
+    width =settings.WIDTH, #same width of window
+    height = utils.height_p(25)
     ) 
 
 top_frame.place(x=0, y=0)
@@ -24,10 +24,31 @@ top_frame.place(x=0, y=0)
 left_frame = Frame(
     root,
     bg = rgb_hack((192, 192, 242)),
-    width =180, 
-    height = 630
+    width =utils.width_p(25),  
+    height = utils.height_p(75)
 )
 
-left_frame.place(x=0, y=90)
+left_frame.place(x=0, y=utils.height_p(25))
+
+
+center_frame = Frame(
+    root,
+    bg = rgb_hack((230, 230, 250)),
+    width =utils.width_p(75),  
+    height = utils.height_p(75)
+
+)
+
+center_frame.place(x=utils.width_p(25), y=utils.height_p(25))
+
+# button1 = Button(
+#     center_frame,
+#     bg = 'light blue',
+#     text = 'First Button'
+# )
+# button1.place(x=720,y=360)
+
+
+
 #Run the window
 root.mainloop() #all code should be between this and root=Tk()
---
+++ utils.py
@@ -0,0 +1,9 @@
+import settings
+
+def height_p(percentage):
+    return (settings.HEIGHT/100) * percentage
+
+# print (height_p(25))
+
+def width_p(percentage):
+    return (settings.WIDTH/100) * percentage
