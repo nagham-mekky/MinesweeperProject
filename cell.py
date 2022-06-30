8d7f1ad2806511e66f4221ee2fc918994f6c5c67
nagham-mekky <nagham.mekky@aucegypt.edu>
2022-06-30 22:40:12 +0200

    Get all cells surrdounding cell clicked on, cell goes red if it is a mine and is clicked on

--- cell.py
+++ cell.py
@@ -17,7 +17,7 @@ class Cell:
     def create_btn_obj(self, location):
         btn = Button(
             location, 
-            #text=f"{self.x},{self.y}",
+            text=f"{self.x},{self.y}",
             width = int(67.5/ settings.GRID_WIDTH),
             height = int(33.75 / settings.GRID_HEIGHT) #SEE IF U CAN MAKE MORE ACCUREATE
         )
@@ -28,10 +28,36 @@ class Cell:
         #make image for flag
     
     def left_click (self, event):
-        print (event)
-        #print ("LEFT")
-        #print ("width ", (utils.width_p(75)))
-        print( "height " , int(33.75 / settings.GRID_HEIGHT))
+        if (self.mine):
+            self.show_mine()#end game, image of mine
+        else:
+            self.show_cell()
+        # print (event)
+        # #print ("LEFT")
+        # #print ("width ", (utils.width_p(75)))
+        # print( "height " , int(33.75 / settings.GRID_HEIGHT))
+    def show_mine(self):
+        #Logic to interrupt game & display message that player lost
+        self.cell_btn_obj.configure(bg='red') #(255,0,0)
+        #Image of mine if mine clicked
+    def get_cell_by_axis (self, x, y):
+        #Return a cell obj based on (x,y)
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    def show_cell(self):
+        cellsSurrounding = [self.get_cell_by_axis(self.x - 1, self.y -1 ),
+        self.get_cell_by_axis(self.x - 1, self.y),
+        self.get_cell_by_axis(self.x - 1, self.y + 1),
+        self.get_cell_by_axis(self.x, self.y - 1),
+        self.get_cell_by_axis(self.x + 1, self.y - 1),
+        self.get_cell_by_axis(self.x + 1, self.y),
+        self.get_cell_by_axis(self.x + 1, self.y + 1),
+        self.get_cell_by_axis(self.x, self.y + 1)]
+        
+        cellsSurrounding = [cell for cell in cellsSurrounding if cell is not None]
+        print(cellsSurrounding)
 
     def right_click (self, event):
         print (event)
